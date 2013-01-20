e1e21245f4bf907f0c6acca293b756b6810b3d1e
Akhil Wali <akhil.wali.10@gmail.com>
2013-01-20 13:09:45 +0530

    Linked to gravatar. Added more CSS.

---
+++ _plugins/to_gravatar.rb
@@ -0,0 +1,26 @@
+require 'digest/md5'
+
+module Jekyll
+  module GravatarFilter
+    def to_gravatar(input)
+      "http://www.gravatar.com/avatar/#{hash(input)}#{size}"
+    end
+
+    def to_secure_gravatar(input)
+      "https://secure.gravatar.com/avatar/#{hash(input)}#{size}"
+    end
+
+    private :hash
+
+    def hash(email)
+      email_address = email ? email.downcase.strip : ''
+      Digest::MD5.hexdigest(email_address)
+    end
+
+    def size
+      "?size=800"
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::GravatarFilter)
