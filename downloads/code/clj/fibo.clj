948a2a0a1b9e6f7150687c27cdc3af6799944703
Akhil Wali <akhil.wali.10@gmail.com>
2016-12-29 01:42:50 +1300

    Site updated on 29/12/2016 01:42

--- downloads/code/clj/fibo.clj
+++
@@ -1,9 +0,0 @@
-(def fibo-seq
-  (map first 
-    (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
-
-(defn fibo-list [n]
-  (take n fibo-seq))
-
-(defn fibo-last [n]
-  (last (fibo-list n)))
--- downloads/code/scala/List.scala
+++
@@ -1,6 +0,0 @@
-object List {
-
-  // ...
-
-  def apply[A](xs: A*): List[A] = xs.toList
-}
