042ef93839bcbe8f7b2ef37674beea52ec51c900
Akhil Wali <akhil.wali.10@gmail.com>
2018-05-14 22:37:21 +1200

    Added turbolinks

--- resources/templates/themes/blue/js/darth10.github.io.js
+++ resources/templates/themes/blue/js/darth10.github.io.js
@@ -1,3 +1,19 @@
 (function () {
-  hljs.initHighlightingOnLoad();
+  function initHighlighting() {
+    if ((typeof Turbolinks === 'undefined') && initHighlighting.called)
+      return;
+    initHighlighting.called = true;
+
+    var blocks = document.querySelectorAll('pre code');
+    blocks.forEach.call(blocks, hljs.highlightBlock);
+  };
+
+  function addHighlightingListener(e) {
+      document.addEventListener(e, initHighlighting, false);
+  };
+
+  addHighlightingListener('turbolinks:load');
+  addHighlightingListener('turbolinks:render');
+  addHighlightingListener('DOMContentLoaded');
+  addHighlightingListener('load');
 })();
