32efbed2ed77d7be0b1d9a9b9079f493cdd68989
Akhil Wali <akhil.wali.10@gmail.com>
2020-04-28 10:18:44 +1200

    Use cryogen-core.markup/Markup protocol

--- src/clj/darth10/github/io/markup.clj
+++ src/clj/darth10/github/io/markup.clj
@@ -1,16 +1,12 @@
 (ns darth10.github.io.markup
   (:require [markdown.core :refer [md-to-html-string]]
+            [cryogen-core.markup :as m]
             [clojure.string :as s])
   (:import org.asciidoctor.Asciidoctor$Factory
            java.util.Collections))
 
-(defprotocol Markup
-  (dir [this])
-  (ext [this])
-  (render-fn [this]))
-
 (defn- markdown []
-  (reify Markup
+  (reify m/Markup
     (dir [this] "md")
     (ext [this] ".md")
     (render-fn [this]
@@ -28,10 +24,10 @@
     (render-fn [this]
       (fn [rdr]
         (.convert (Asciidoctor$Factory/create)
-         (->> (java.io.BufferedReader. rdr)
-              (line-seq)
-              (s/join "\n"))
-         (Collections/emptyMap))))))
+                  (->> (java.io.BufferedReader. rdr)
+                       (line-seq)
+                       (s/join "\n"))
+                  (Collections/emptyMap))))))
 
 (defn markups []
   [(markdown) (asciidoc)])
