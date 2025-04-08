4a0adba71f1eb6e1af27826ba23f149bba365ed8
Akhil Wali <akhil.wali.10@gmail.com>
2025-04-08 21:07:25 +1200

    Format `darth10.github.io.js`

--- src/js/darth10.github.io.js
+++ src/js/darth10.github.io.js
@@ -9,14 +9,15 @@ function initHighlighting() {
 
 // This is a workaround for an issue with Turbolinks.
 // See: https://github.com/turbolinks/turbolinks/issues/75
-function checkAndPreventOnAnchor (event) {
+function checkAndPreventOnAnchor(event) {
   if (event.target.getAttribute('href').charAt(0) === '#') {
-    setTimeout(function() {
+    setTimeout(function () {
       // This is needed to scroll in-page anchor links below the
       // Bootstrap NavBar.
       if (window.location.hash) {
         scrollBy(0, -66);
-      }}, 10);
+      }
+    }, 10);
     return event.preventDefault();
   }
 };
