af07f0011932a2c3dfb2e63b6b5b4f2b2ead457a
Akhil Wali <akhil.wali.10@gmail.com>
2026-07-31 15:43:55 +1200

    Use livereload-js instead of livereload

--- src/clj/darth10/github/io/server.clj
+++ src/clj/darth10/github/io/server.clj
@@ -54,7 +54,7 @@
 (defn reload-handler [request]
   (if (jetty/ws-upgrade-request? request)
     (reload/ws-handler request)
-    ;; Serve livereload.js from npm modules.
+    ;; Serve livereload-js from npm modules.
     {:body (slurp (path "node_modules" "livereload-js" "dist"
                         "livereload.min.js"))
      :status 200}))
